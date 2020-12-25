d22c0553911e4ccfd575f34263e81859cf0ccb65
ntwmac-dev <ntwmac.dev@gmail.com>
2020-12-25 01:06:52 +0000

    Added "Player" enum for better code readability

--- connect-four/CFourLogic.cs
+++ connect-four/CFourLogic.cs
@@ -1,12 +1,13 @@
-﻿/** ConnectFour.cs */
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace connect_four {
-    /** Defines the UI manipulation and game logic */
-    class CFourLogic {
+    /** 
+     * Defines the UI controls and game logic for a connect four game 
+     */
+    public class CFourLogic {
 
         /*      Const       */
         private const int COLUMN_MAX = 7;
@@ -15,6 +16,7 @@ namespace connect_four {
         private static readonly SolidColorBrush RGB_YELLOW = new SolidColorBrush(Color.FromRgb(0xff, 0xff, 0x00));
         private static readonly SolidColorBrush RGB_DARK = new SolidColorBrush(Color.FromRgb(0x33, 0x33, 0x33));
 
+
         /*      UI Elements     */
         private static List<List<Border>> lsBorders;
         private static List<StackPanel> lsStackPanels;
@@ -22,11 +24,16 @@ namespace connect_four {
         private static TextBlock txbWinner;
 
         /*      Game Variables      */
-        private bool currentPlayer = true;   // true = Red team, false = Yellow team
+        private enum Player {
+            NONE,                       // NONE must be first enum (defaults for grid population)
+            RED,
+            YELLOW
+        }
+        private Player currentPlayer;   // tracks the current player
         
         /** Game grid */
         private readonly int[] columnCounter;      // Increments a column upon player selection
-        private readonly int[,] arGameGrid;     // Tracks each teams selection
+        private readonly Player[,] arGameGrid;     // Tracks each teams selection
         /*   -----------------------
          * 5 | 05 15 25 35 45 55 65 |
          * 4 | 04 14 24 34 44 54 64 |
@@ -40,24 +47,22 @@ namespace connect_four {
 
         /** Constructor */
         public CFourLogic() {
-            currentPlayer = true;
+            currentPlayer = Player.RED;
             columnCounter = new int[COLUMN_MAX];
-            arGameGrid = new int[COLUMN_MAX, ROW_MAX];
+            arGameGrid = new Player[COLUMN_MAX, ROW_MAX];
         }
 
         /** Updates UI & arGameGrid */
         public void checkUserInput(int column) {
+
             int row = columnCounter[column];
             // Validate row is under max index
             if (row != ROW_MAX) {
+
                 // Update grid and UI with users input
-                if (currentPlayer) {
-                    arGameGrid[column, row] = 1;
-                    updateUIGrid(column, columnCounter[column], RGB_RED);
-                } else {
-                    arGameGrid[column, row] = 3;
-                    updateUIGrid(column, columnCounter[column], RGB_YELLOW);
-                }
+                arGameGrid[column, row] = currentPlayer;
+                updateUIGrid(column, row, currentPlayer);
+
                 // Invoke game logic
                 runGameLogic(column);
                 columnCounter[column]++;
@@ -68,17 +73,17 @@ namespace connect_four {
             }
         }
 
-        private bool toggleCurrentPlayer(bool isRedTeam) {
+        /** Toggle UI to show which team is due to play next */
+        private Player toggleCurrentPlayer(Player team) {
             
-            // Toggle UI to show which team is due to play next
-            if (isRedTeam) {
+            if (team.Equals(Player.RED)) {
+                team = Player.YELLOW;
                 brdCurrentPlayer.Background = RGB_YELLOW;
-
             } else {
+                team = Player.RED;
                 brdCurrentPlayer.Background = RGB_RED;
             }
-            // Swap the team for the game logic
-            return !isRedTeam;
+            return team;
         }
 
         /** Check each direction on the player's selection for a victory (4 in a line) */
@@ -109,7 +114,7 @@ namespace connect_four {
             // Diagonal Left ( \ )
             sumScore = 0;
             for (findPositionLimit(true); posY < ROW_MAX; posY++, posX--) {
-                if (posX > 0 && posY < ROW_MAX) {
+                if (posX > -1 && posY < ROW_MAX) {
                     compareScore(arGameGrid[posX, posY]);
                 }
             }
@@ -118,13 +123,13 @@ namespace connect_four {
             void findPositionLimit(bool invert) {
                 posX = column;
                 posY = row;
-                // Diagonal Right
+                // Diagonal Right ( / )
                 if (!invert) {
                     while (posX > 0 && posY > 0) {
                         posX--;
                         posY--;
                     }
-                // Diagonal Left
+                // Diagonal Left ( \ )
                 } else {
                     while (posX < COLUMN_MAX - 1 && posY > 0) {
                         posX++;
@@ -134,28 +139,18 @@ namespace connect_four {
             }
 
             /** (Nested) Compare the score against the index */
-            void compareScore(int gridIndex) {
+            void compareScore(Player gridIndex) {
 
-                // Validates the grid index is the same player
-                bool identifyGridSelection = gridIndex switch {
-                    1 => true,              // Red
-                    3 => false,             // Yellow
-                    _ => !currentPlayer     // Empty
-                };
-
-                if (identifyGridSelection != currentPlayer) {
-                    sumScore = 0;
+                // Increment score
+                if (gridIndex.Equals(currentPlayer)) {
+                    sumScore++;
                 } else {
-                    sumScore = sumScore + gridIndex;
+                    sumScore = 0;
                 }
 
                 // Check for a victory condition
                 if (sumScore == 4) {
-                    // Red team wins
-                    showUIWinner(true);
-                } else if (sumScore == 12) {
-                    // Yellow team wins
-                    showUIWinner(false);
+                    showUIWinner(currentPlayer);
                 }
             }
         }
@@ -175,7 +170,7 @@ namespace connect_four {
             // Reset grid
             for (int i = 0; i < COLUMN_MAX; i++) {
                 for (int j = 0; j < ROW_MAX; j++) {
-                    updateUIGrid(i, j, RGB_DARK);
+                    updateUIGrid(i, j, Player.NONE);
                 }
             }
             // Reset player to default
@@ -185,17 +180,20 @@ namespace connect_four {
         }
 
         /** Sets the selected grid element to the specified color */
-        public static void updateUIGrid(int innerIndex, int outerIndex, SolidColorBrush colorBrush) {
-            lsBorders[innerIndex][outerIndex].Background = colorBrush;
+        private static void updateUIGrid(int innerIndex, int outerIndex, Player team) {
+            
+            SolidColorBrush selectColorBrush = team switch {
+                Player.RED => RGB_RED,
+                Player.YELLOW => RGB_YELLOW,
+                _ => RGB_DARK        
+            };
+
+            lsBorders[innerIndex][outerIndex].Background = selectColorBrush;
         }
 
         /** Disables user input and updates UI with the games winner */
-        private static void showUIWinner(bool isRedTeam) {
-            if (isRedTeam) {
-                txbWinner.Text = "Red Team Wins!";
-            } else {
-                txbWinner.Text = "Yellow Team Wins!";
-            }
+        private static void showUIWinner(Player team) {
+            txbWinner.Text = $"{team} Team Wins!";
             txbWinner.Visibility = Visibility.Visible;
             enableUIButtons(false);
         }
